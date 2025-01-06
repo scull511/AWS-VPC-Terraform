eb02f281847895f8cde852c7cd8e0f325b30776c
Simon Cull <simoncull@gmail.com>
2025-01-06 21:33:01 +0000

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instance_a_sc" {
   ami           = "ami-019374baf467d6601"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_a_sc.id
+  vpc_security_group_ids = [aws_security_group.public_sg_sc.id]
 
   tags = {
     Name = "Test Instance A - SC"
@@ -15,6 +16,7 @@ resource "aws_instance" "instance_b_sc" {
   ami           = "ami-019374baf467d6601"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_b_sc.id
+  vpc_security_group_ids = [aws_security_group.public_sg_sc.id]
 
   tags = {
     Name = "Test Instance B - SC"
